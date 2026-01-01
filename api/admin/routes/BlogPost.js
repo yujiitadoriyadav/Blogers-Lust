006c9ba03da30685874320ccc8bbcd7a765bbaf9
Dev Pratap <pratapyash328@gmail.com>
2026-01-01 22:29:49 +0530

    make blogpost router foradmin

--- api/admin/routes/BlogPost.js
+++ api/admin/routes/BlogPost.js
@@ -2,6 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 //get all blog
-router.get()
+router.get('/',(req,res)=>{
+    res.status(200).json({
+        blogs:'array of blogs'
+    })
+})
 
+//get a single blog
+router.get('/:id',(req,res)=>{
+    res.status(200).json({
+        blogDetail:req.params.id + ' blog detail'
+    })
+})
 module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express =require('express');
+const app =express();
 const adminBlogPostRoute = require('./api/admin/routes/BlogPost')
 
 
